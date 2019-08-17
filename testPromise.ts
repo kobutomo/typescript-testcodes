605d27845b129eb6f60e4811c61dee499f033db4
tomoro <datz.knave@gmail.com>
2019-08-18 00:00:29 +0900

    reject サンプル

--- testPromise.ts
+++ testPromise.ts
@@ -1,12 +1,27 @@
 const timeout = (second: number) => {
-  return new Promise<string>(resolve => {
-    setTimeout(() => resolve(second.toString() + "秒経過"), second * 1000)
+  return new Promise<string>((resolve, reject) => {
+    if (second < 2) {
+      setTimeout(() => resolve(second.toString() + "秒経過"), second * 1000)
+    }
+    else {
+      setTimeout(() => reject("timeover"), second * 1000)
+
+    }
   })
 }
+const oneSecond = async () => {
+  console.log(await timeout(1).catch(err => err))
+}
+const twoSeconds = async () => {
+  console.log(await timeout(2).catch(err => err))
+}
+const threeSeconds = async () => {
+  console.log(await timeout(3).catch(err => err))
+}
 const testFunc = async () => {
-  const [one, two, three] = await Promise.all([timeout(1), timeout(2), timeout(3)])
-  console.log(one)
-  console.log(two)
-  console.log(three)
+  console.log("start")
+  oneSecond()
+  twoSeconds()
+  threeSeconds()
 }
 testFunc()
