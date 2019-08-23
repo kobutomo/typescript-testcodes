24a55d265248644130c3bbf60ffe5b8407fccca5
tomoro <n.tomoro13@gmail.com>
2019-08-23 23:21:35 +0900

    非同期処理についてお勉強

--- src/testPromise.ts
+++ src/testPromise.ts
@@ -1,16 +1,36 @@
 const timeout = (second: number) => {
-  return new Promise<void>((resolve, reject) => {
+  //<() => string>はresolveの引数の型
+  return new Promise<() => string>((resolve, reject) => {
     if (second < 4) {
-      setTimeout(() => resolve(console.log(second.toString() + "秒経過")), second * 1000)
+      setTimeout(() => resolve(() => {
+        return second.toString()
+      })
+        , second * 1000)
     }
     else {
-      setTimeout(() => reject(console.log("timeover")), second * 1000)
+      // rejectの引数はvoidである必要はない。
+      // この場合stringが引数に入ってるが問題ない。
+      // rejectの引数に型アノテーションできない？
+      // 我々はその謎を解明すべくアマゾンの奥地へ向かった。
+      // 参考:https://orgachem.hatenablog.com/entry/2016/05/25/185157
+      setTimeout(() => reject("hey"), second * 1000)
     }
   })
 }
 
+// async functionが値を返す場合、
+// 例えばstrirng型を返す場合、関数の型は
+// () => stringではなく() => Promise<string>となる。
+// stringをreturnしているが、厳密にいうとresolveで
+// stringを引数にとるPromiseを返している。
+// つまり返り値を使いたい場合awaitが必要。
 const outputTimeLog = async (second: number) => {
-  await timeout(second).catch(err => err)
+  try {
+    const data = await timeout(second)
+    console.log(data())
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const testFunc = async () => {
